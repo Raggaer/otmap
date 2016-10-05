ca2155e724ddf2cbc70a20e31ec2c221ed45bde6
Raggaer <nakotoffana@gmail.com>
2016-10-05 16:16:21 +0200

    Return information

--- parser.go
+++ parser.go
@@ -8,38 +8,48 @@ import (
 	"os"
 )
 
+type Map struct {
+	Width    uint16
+	Height   uint16
+	Header   uint32
+	Property uint8
+}
+
 // Parse parses the given .otbm file
-func Parse(filepath string) error {
+func Parse(filepath string) (*Map, error) {
 	file, err := os.Open(filepath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
 	var identifier [4]byte
 	if err := binary.Read(reader, binary.LittleEndian, &identifier); err != nil {
-		return err
+		return nil, err
 	}
 	if !bytes.Equal(identifier[:4], []byte{'\x00', '\x00', '\x00', '\x00'}) && !bytes.Equal(identifier[:4], []byte{'O', 'T', 'B', 'M'}) {
-		return fmt.Errorf("Unsupported file identifier: %v", identifier)
+		return nil, fmt.Errorf("Unsupported file identifier: %v", identifier)
 	}
 	root := Node{}
 	if err := root.Parse(reader); err != nil {
-		return err
+		return nil, err
 	}
 	var property uint8
 	if err := binary.Read(root.data, binary.LittleEndian, &property); err != nil {
-		return err
+		return nil, err
 	}
 	if property != 0 {
-		return fmt.Errorf("Unable to read OTBM root property. got %v exptected %v", property, 0)
+		return nil, fmt.Errorf("Unable to read OTBM root property. got %v exptected %v", property, 0)
 	}
 	var headerVersion uint32
 	if err := binary.Read(root.data, binary.LittleEndian, &headerVersion); err != nil {
-		return err
+		return nil, err
 	}
 	if headerVersion > 3 {
-		return fmt.Errorf("Unsupported OTBM version. got %v expected %v", headerVersion, "> 3")
+		return nil, fmt.Errorf("Unsupported OTBM version. got %v expected %v", headerVersion, "> 3")
 	}
-	return nil
+	currentMap := &Map{}
+	currentMap.Header = headerVersion
+	currentMap.Property = property
+	return currentMap, nil
 }
--- parser_test.go
+++ parser_test.go
@@ -3,7 +3,7 @@ package otmap
 import "testing"
 
 func TestParser(t *testing.T) {
-	if err := Parse("C:/Users/ragga/Desktop/test.otbm"); err != nil {
+	if _, err := Parse("C:/Users/ragga/Desktop/test.otbm"); err != nil {
 		t.Error(err)
 	}
 }
